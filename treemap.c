ff596242fd30cead2d2a47c4e646677b712baca8
Replit user <>
2023-04-22 05:45:45 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -218,11 +218,11 @@ Pair *nextTreeMap(TreeMap *tree)
   } 
   else // es el ultimo de la rama derecha, pasamos a la izquierda
   {
-    
+    TreeNode* parent = current->parent;
     while ( current->parent != NULL && current ==  current->parent->right)
     {
-      current =  current->parent;
-       current->parent =  current->parent->parent;
+      current = parent;
+      parent = parent->parent;
     }
     current =  current->parent;
   }
